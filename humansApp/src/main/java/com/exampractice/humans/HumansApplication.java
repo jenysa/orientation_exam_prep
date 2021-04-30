77a496076a6c1110437b32148573d99d99d20e50
Amanda Jenys <jenys.amanda@gmail.com>
2021-04-30 11:43:37 +0200

    create main page and first endpoint, connect to mysql database

---
+++ humansApp/src/main/java/com/exampractice/humans/HumansApplication.java
@@ -0,0 +1,13 @@
+package com.exampractice.humans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumansApplication.class, args);
+    }
+
+}
---
+++ humansApp/src/test/java/com/exampractice/humans/HumansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampractice.humans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
